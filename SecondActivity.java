89eca9f4b7c9d984b81ed94d275368c0bc094a0b
Abhinandan <abhinandanverma551@gmail.com>
2023-10-29 02:44:52 +0530

    Update SecondActivity.java
    updated secondActivity again

--- SecondActivity.java
+++ SecondActivity.java
@@ -2,5 +2,8 @@ public class SecondActivity{
 
     public class void main(String[] args){
         System.out.println("Namaste india \n How are you?");
+
+        for(int i=0;i<10;i++){
+            system.out.println("Jai Bharat");
     }
 }
