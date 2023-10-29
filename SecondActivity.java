5f04a11249615d29e68a4c0dbcff369caf3834bb
abhinandan-verma <abhinandanverma551@gmail.com>
2023-10-29 10:23:58 +0530

    adapter App commit

--- SecondActivity.java
+++ SecondActivity.java
@@ -2,8 +2,5 @@ public class SecondActivity{
 
     public class void main(String[] args){
         System.out.println("Namaste india \n How are you?");
-
-        for(int i=0;i<10;i++){
-            system.out.println("Jai Bharat");
     }
 }
